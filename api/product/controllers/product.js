881ef0106368028fbc18442d6dfdee301ecb29a1
Jason Yapri <jason@inergi.id>
2021-09-23 21:01:17 +0700

    Override product controller

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -1,8 +1,16 @@
 'use strict';
 
+const { default: createStrapi } = require("strapi");
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find (ctx) {
+    const products = await strapi.services.product.find(ctx.query)
+
+    return products.map(product => product.title)
+  }
+};
